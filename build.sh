696339e258b9decb6e53f4bd59289b77269565a0
DR6677 <n.leelaranga@gmail.com>
2025-07-17 18:14:22 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-# Instruction 1: Use 'pip' to install all the Python packages.
-pip install -r requirements.txt
-
-# Instruction 2: Now, use 'apt-get' to install the system program.
-apt-get update && apt-get install -y libreoffice
