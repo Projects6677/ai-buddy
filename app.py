c6ce0adbd0749668d8b29f88a7575be8c1d82b9e
DR6677 <n.leelaranga@gmail.com>
2025-07-19 22:42:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ from grok_ai import (
     edit_email_body,
     write_email_body_with_grok
 )
-from youtube_summarizer import summarize_youtube_video
 from email_sender import send_email
 from cricket import get_live_match_list, get_score_for_match
 
