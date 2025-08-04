bbbf1fc0eca0df328c746c304816ae03cd05cc92
DR6677 <n.leelaranga@gmail.com>
2025-08-04 17:58:16 +0530

    Update messaging.py

--- messaging.py
+++ messaging.py
@@ -33,10 +33,10 @@ def send_template_message(to, template_name, components=[]):
         "Content-Type": "application/json"
     }
     # --- MODIFICATION START ---
-    # Changed language code to "en_IN" to match your Indian English template
+    # Standardized language code to en_US for all templates
     template_data = {
         "name": template_name,
-        "language": {"code": "en_IN"}
+        "language": {"code": "en_US"}
     }
     # --- MODIFICATION END ---
     if components:
