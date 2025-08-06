e9d6136fe1e6058efe4c3bc401e40e7ef7068157
DR6677 <n.leelaranga@gmail.com>
2025-08-06 21:23:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,11 +88,9 @@ def create_or_update_user_in_db(sender_number, data):
     """Saves or updates a user's data in the database."""
     users_collection.update_one({"_id": sender_number}, {"$set": data}, upsert=True)
 
-# --- MODIFICATION START ---
 def get_all_users_from_db():
     """Fetches all users (ID, name, and connection status) from the database."""
     return users_collection.find({}, {"_id": 1, "name": 1, "is_google_connected": 1})
-# --- MODIFICATION END ---
 
 def delete_all_users_from_db():
     """Deletes all user data from the database."""
@@ -584,7 +582,6 @@ def handle_text_message(user_text, sender_number, state):
         user_sessions.pop(sender_number, None)
     elif state == "awaiting_ai":
         response_text = ai_reply(user_text)
-        user_sessions.pop(sender_number, None)
     elif state == "awaiting_translation":
         response_text = translate_with_grok(user_text)
         user_sessions.pop(sender_number, None)
