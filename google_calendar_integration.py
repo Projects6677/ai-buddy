c97511fc693866f83bdbe0fefed0be62b130d1cc
Dhruvin Sai Sajja <n.leelaranga@gmail.com>
2025-08-22 18:54:02 +0530

    Update google_calendar_integration.py

--- google_calendar_integration.py
+++ google_calendar_integration.py
@@ -11,7 +11,8 @@ SCOPES = [
     'https://www.googleapis.com/auth/calendar.events',
     'https://www.googleapis.com/auth/gmail.readonly',
     'https://www.googleapis.com/auth/gmail.send',
-    'https://www.googleapis.com/auth/drive'
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/spreadsheets'  # <-- ADD THIS LINE
 ]
 REDIRECT_URI = os.environ.get("GOOGLE_REDIRECT_URI", "https-your-app-url.com/google-auth/callback")
 
